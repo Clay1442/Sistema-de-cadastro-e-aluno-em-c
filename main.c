eac9932f8ef429dfff6c096c15d66dc6bf7abda6
Clay1442 <clayjose20@gmail.com>
2024-11-02 18:55:41 -0300

    Inicio

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+    
+    int selecionado; 
+
+   printf("Bem vindo ao painel de cadastro de aluno\n\n");
+   printf("Selecione o que deseja digitando o numero: \n\n 1.Cadastrar \n 2.Listar \n 3.Excluir \n 4.Editar \n 5.Pesquisar \n 6.Desligar \n");
+         scanf("%d", &selecionado);   
+         
+         
+         
+         
+
+system("pause");
+
+
+	return 0;
+}
