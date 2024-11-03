799301f45981b192fc344964af1a98498fbc30aa
Clay1442 <clayjose20@gmail.com>
2024-11-03 17:51:13 -0300

    criação da função listar

--- main.c
+++ main.c
@@ -17,29 +17,64 @@ typedef struct{
 
  void cadastrar(){
 	aluno cadastro;
-	
+	FILE *arquivo= fopen("aluno.txt", "a");
+
+    if (arquivo == NULL){
+    	printf("Erro ao abrir o arquivo.\n");
+    	return;
+	}
 	
 	//Leitura nome
-	printf("Digite o nome do aluno: ");
+	printf("Digite o nome do aluno: \n");
     fgets(cadastro.nome, 100, stdin);
     
     //Leitura da idade
-    printf("\nDigite sua idade:");
+    printf("Digite sua idade:\n");
     scanf("%d", &cadastro.idade);
     getchar(); // Consome o '\n' deixado pelo scanf
     
 	//Leitura do curso   
-    printf("Digite o nome do curso: ");
+    printf("Digite o nome do curso: \n");
 	fgets(cadastro.curso, 200, stdin);   
 	
 	//Leitura da matricula 
      printf("Digite a matricula do aluno:");
      scanf("%d", &cadastro.matricula);
-	   
-    printf("Nome: %s\n Idade: %d\n  Curso: %s\n Matricula: %d\n", cadastro.nome, cadastro.idade, cadastro.curso, cadastro.matricula);
+	 
+	//print resultado  
+    printf("\nNome: %s Idade: %d\n Curso: %s Matricula: %d\n", cadastro.nome, cadastro.idade, cadastro.curso, cadastro.matricula);
+    
+    //Salvar no arquivo
+	fprintf(arquivo, "%s%d\n%s%d\n", cadastro.nome, cadastro.idade, cadastro.curso, cadastro.matricula);
+	
+	printf("\nSalvo com sucesso\n");   
+	fclose(arquivo);
 	
 }
 
+void listar(){
+ FILE *arquivo = fopen("aluno.txt", "r");
+ aluno cadastro;
+ 
+ if (arquivo == NULL){
+ 		printf("Erro ao abrir o arquivo.\n");
+    	return;
+ }
+	printf("Lista de Alunos Cadastrados: \n");
+	 while (fgets(cadastro.nome, 100, arquivo) != NULL) {
+        fscanf(arquivo, "%d\n", &cadastro.idade);  
+		fgets(cadastro.curso, 200, arquivo);
+	    fscanf(arquivo, "%d\n", &cadastro.matricula);
+
+        // Imprime os dados do aluno
+        printf("\n\tNome: %s", cadastro.nome);
+        printf("\n\tIdade: %d\n", cadastro.idade);
+        printf("\tCurso: %s", cadastro.curso);
+        printf("\tMatricula: %d\n", cadastro.matricula);
+    }
+	
+	
+}
 
 
 
@@ -48,7 +83,7 @@ int main(int argc, char *argv[]) {
     int selecionado; //variavel que seleciona a função  
     
 do {    
-   printf("Bem vindo ao painel de cadastro de aluno\n");
+   printf("\nBem vindo ao painel de cadastro de aluno\n");
    printf("Selecione o que deseja digitando o numero: \n\n 1.Cadastrar \n 2.Listar \n 3.Pesquisar \n 4.Editar \n 5.Excluir \n 6.Desligar \n");
          scanf("%d", &selecionado);   
           getchar(); // Consome o '\n' deixado pelo scanf
@@ -57,6 +92,9 @@ do {
        case 1:
 	      cadastrar();
 		  break;
+		case 2:
+           listar();
+			break;
 		default:
 		   printf("selecao inexistente.\n");
 		    break;
